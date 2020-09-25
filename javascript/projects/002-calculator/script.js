dd5040ad4417f8cbb73db2406382052b8e010859
=mustafa0624 <mustafa.yucel0624@gmail.com>
2020-09-25 20:14:03 +0200

    feat:add calculator js  first section

---
+++ javascript/projects/002-calculator/script.js
@@ -0,0 +1,9 @@
+document.getElementsByClassName("button").addEventListener("click",screen());
+var deger = document.getElementsByClassName("button").value;
+
+function screen(){
+    
+    document.getElementById("form-input").innerHTML=deger;
+}
+
+
