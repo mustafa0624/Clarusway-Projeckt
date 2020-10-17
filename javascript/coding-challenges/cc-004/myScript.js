65937b96b16b7b7209557915d3b9734ac930fdf2
=mustafa0624 <mustafa.yucel0624@gmail.com>
2020-10-17 10:56:36 +0200

    coding challenge 4 mergeArray is done

---
+++ javascript/coding-challenges/cc-004/myScript.js
@@ -0,0 +1,14 @@
+function Merge(array1, array2) {
+    const resultArray = [...array1, ...array2];
+    const neww = []
+    for (let i = 0; i < resultArray.length; i++) {
+        if (resultArray[i] == "") delete i;
+        else {
+            neww.push(resultArray[i])
+        }
+    }
+    console.log(neww.sort((a,b)=>a-b))
+
+
+}
+Merge([12, 14, [], 16], [11, 13, 17])
